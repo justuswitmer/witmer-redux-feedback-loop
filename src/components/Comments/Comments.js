7aa26bf1865e7e6f47fbc632bb95e85f1420b0ae
Justus Witmer <justus.witmer@gmail.com>
2020-09-28 10:28:54 -0500

    changed input to Textfield

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Button, Paper } from '@material-ui/core';
+import { Button, Paper, TextField } from '@material-ui/core';
 
 class Comments extends Component {
 
@@ -36,27 +36,34 @@ class Comments extends Component {
         elevation={3}
       >
         <h1>Any comments you want to leave?</h1>
-        <div className='inputField'>
-          <h4>Comments?</h4>
-          <input
-            type='text'
-            onChange={this.handleSelect}
-          />
+        <div className="commentsDiv">
+          <div className="commentsText">
+            <TextField
+              multiline
+              rowsMax={4}
+              id="outlined-basic"
+              label="Comments?"
+              variant="outlined"
+              onChange={this.handleSelect}
+            />
+          </div>
+          <div className="commentsButtons">
+            <Button
+              id="formatBtn"
+              variant="contained"
+              color="primary"
+              onClick={this.goBack}
+            >Back
+            </Button>
+            <Button
+              id="formatBtn"
+              variant="contained"
+              color="primary"
+              onClick={this.handleSubmit}
+            >Next
+        </Button>
+          </div>
         </div>
-        <Button
-          id="formatBtn"
-          variant="contained"
-          color="primary"
-          onClick={this.goBack}
-        >Back
-        </Button>
-        <Button
-          id="formatBtn"
-          variant="contained"
-          color="primary"
-          onClick={this.handleSubmit}
-        >Next
-        </Button>
       </Paper>
     );
   }
